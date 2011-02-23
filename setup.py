89d9c3007c0bf3bfe47bbe90b5094d3529f418a3
Darian Moody <mail@djm.org.uk>
2011-02-23 11:49:19 +0000

    Updated filename of README in setup.py so install doesn't bomb out.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup, find_packages
 
-f = open(os.path.join(os.path.dirname(__file__), 'README'))
+f = open(os.path.join(os.path.dirname(__file__), 'README.md'))
 readme = f.read()
 f.close()
 
